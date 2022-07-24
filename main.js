69ac2297d34604e3d89448eeb3bb5b225eca667f
angulini <seenfria@hotmail.com>
2022-07-25 00:29:58 +0200

    añadido la carpeta src con add.html y js y index.html y js, añadido los estilos de ambos en assets y la imagen tambien

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-// main.js
+//* main.js
 
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+//* Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
+const shell = require('electron').shell
+
 
 const createWindow = () => {
   // Create the browser window.
@@ -12,32 +14,64 @@ const createWindow = () => {
     height: 600
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  //* and load the index.html of the app.
+  mainWindow.loadFile('src/index.html')
+  mainWindow.webContents.openDevTools()
+  //* Open the DevTools.
+  //* mainWindow.webContents.openDevTools()
+  let menu = Menu.buildFromTemplate([
+    {
+      //* aqui se crea la etiqueta de la barra de menu principal
+      //* label es para nombrar las cosas
+      label: 'Menu',
+      //* etiqueta de submenu
+      submenu: [
+        //* se pone lo que se quiera aparececer dentro del array con objetos
+          {label: 'Adjust Notification Value'},
+          {
+            label: 'CoinMarketCap',
+            click() {
+              //* abre una pagina externa al programa
+              shell.openExternal('http://coinmarketcap.com')
+            }
+          },
+          //* el type identifica lo que queremos poner como por ejemplo un separador 
+          {type: 'separator'},
+          {
+            label: 'Exit',
+            click() {
+              app.quit()
+            }
+          }
+        
+      ]
+    },
+    {
+      label: 'Info'
+    }
+  ])
+  Menu.setApplicationMenu(menu);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Algunas APIs pueden solamente ser usadas despues de que este evento ocurra.
+//* This method will be called when Electron has finished
+//* initialization and is ready to create browser windows.
+//* Algunas APIs pueden solamente ser usadas despues de que este evento ocurra.
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+    //* On macOS it's common to re-create a window in the app when the
+    //* dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+//* Quit when all windows are closed, except on macOS. There, it's common
+//* for applications and their menu bar to stay active until the user quits
+//* explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. Tu también puedes ponerlos en archivos separados y requerirlos aquí.
+//* In this file you can include the rest of your app's specific main process
+//* code. Tu también puedes ponerlos en archivos separados y requerirlos aquí.
