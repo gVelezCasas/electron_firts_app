fb9ff775d738c488c810d10aef60db6405d6ee1e
angulini <seenfria@hotmail.com>
2022-07-25 19:19:22 +0200

    se ha añadido utilidad al boton update y ya sale la segunda pestaña

--- main.js
+++ main.js
@@ -11,7 +11,12 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
   })
 
   //* and load the index.html of the app.
